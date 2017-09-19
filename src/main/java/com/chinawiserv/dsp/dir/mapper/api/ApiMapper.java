4fc80db0c1d392c2500fded64c1f55fb4e7c36bb
gongj <gongj@chinawiserv,com>
2017-09-19 17:57:51 +0800

    共享服务接口代码更新

---
+++ src/main/java/com/chinawiserv/dsp/dir/mapper/api/ApiMapper.java
@@ -0,0 +1,22 @@
+package com.chinawiserv.dsp.dir.mapper.api;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA
+ * Description:
+ * Author:GongJun
+ * Date:2017/9/19
+ * Time:15:47
+ * Chinawiserv Technologies Co., Ltd.
+ */
+public interface ApiMapper{
+
+    List<Map<String,Object>> test(Map<String,Object> paramMap);
+
+    List<Map<String,Object>> getSubDirectoryById(Map<String,Object> paramMap);
+
+    List<Map<String,Object>> getSubDataItemById(Map<String,Object> paramMap);
+
+}
