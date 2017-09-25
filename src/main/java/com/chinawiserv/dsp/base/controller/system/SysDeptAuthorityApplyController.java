5c1ebf0472b4e259a8554e5cfd2b77f4d66ca97b
曾鹏志 <zengpzh@chinawiserv.com>
2017-09-25 18:36:48 +0800

    用户部门数据申请

--- src/main/java/com/chinawiserv/dsp/base/controller/system/SysDeptAuthorityApplyController.java
+++ src/main/java/com/chinawiserv/dsp/base/controller/system/SysDeptAuthorityApplyController.java
@@ -29,121 +29,61 @@ import java.util.Map;
  * @since 2017-09-25
  */
 @Controller
-@RequestMapping("/sysDeptAuthorityApply")
-//todo 将所有的XXX修改为真实值
+@RequestMapping("/system/deptAuthorityApply")
 public class SysDeptAuthorityApplyController extends BaseController {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private ISysDeptAuthorityApplyService service;
 
-    @RequiresPermissions("XXX:XXX:list")
+    @RequiresPermissions("system:deptAuthorityApply:list")
     @RequestMapping("")
-    public  String init(@RequestParam Map<String , Object> paramMap){
-		setCurrentMenuInfo(paramMap);
-    	return "XXX/XXX/XXXList";
+    public String init(@RequestParam Map<String, Object> paramMap) {
+        setCurrentMenuInfo(paramMap);
+        return "system/deptAuthorityApply/deptAuthorityApplyList";
     }
 
     /**
      * 分页查询数据权限申请表
      */
-    @RequiresPermissions("XXX:XXX:list")
+    @RequiresPermissions("system:deptAuthorityApply:list")
     @RequestMapping("/list")
     @ResponseBody
-    public PageResult list(@RequestParam Map<String , Object> paramMap){
-		PageResult pageResult = new PageResult();
-		try {
-		    Page<SysDeptAuthorityApplyVo> page = service.selectVoPage(paramMap);
-		    pageResult.setPage(page);
-		} catch (Exception e) {
-		    pageResult.error("分页查询数据权限申请表出错");
-		    logger.error("分页查询数据权限申请表出错", e);
-		}
-		return pageResult;
+    public PageResult list(@RequestParam Map<String, Object> paramMap) {
+        PageResult pageResult = new PageResult();
+        try {
+            Page<SysDeptAuthorityApplyVo> page = service.selectVoPage(paramMap);
+            pageResult.setPage(page);
+        } catch (Exception e) {
+            pageResult.error("分页查询数据权限申请表出错");
+            logger.error("分页查询数据权限申请表出错", e);
+        }
+        return pageResult;
     }
 
     /**
      * 新增数据权限申请表
      */
-    @RequiresPermissions("XXX:XXX:add")
+    @RequiresPermissions("system:deptAuthorityApply:add")
     @RequestMapping("/add")
-    public  String add(){
-		return "XXX/XXX/XXXAdd";
-    }
+    public String add() {return "system/deptAuthorityApply/deptAuthorityApplyAdd";}
 
     /**
      * 执行新增
      */
-    @RequiresPermissions("XXX:XXX:add")
+    @RequiresPermissions("system:deptAuthorityApply:add")
     @Log("创建数据权限申请表")
     @RequestMapping("/doAdd")
     @ResponseBody
-    public HandleResult doAdd(SysDeptAuthorityApplyVo entity){
-		HandleResult handleResult = new HandleResult();
-		try {
-		    service.insertVO(entity);
-		    handleResult.success("创建数据权限申请表成功");
-		} catch (Exception e) {
-		    handleResult.error("创建数据权限申请表失败");
-		    logger.error("创建数据权限申请表失败", e);
-		}
-		return handleResult;
-    }
-
-    /**
-     * 删除数据权限申请表
-     */
-    @RequiresPermissions("XXX:XXX:delete")
-    @Log("删除数据权限申请表")
-    @RequestMapping("/delete")
-    @ResponseBody
-    public HandleResult delete(@RequestParam String id){
-		//todo 逻辑删除
-    	//service.deleteById(id);
-		return new HandleResult().success("删除数据权限申请表成功");
-    }
-
-    /**
-     * 编辑数据权限申请表
-     */
-    @RequiresPermissions("XXX:XXX:edit")
-    @RequestMapping("/edit")
-    public  String edit(@RequestParam String id,Model model){
-		model.addAttribute("id",id);
-		return "XXX/XXX/XXXEdit";
-    }
-
-    @RequiresPermissions("XXX:XXX:edit")
-    @RequestMapping("/editLoad")
-    @ResponseBody
-    public  HandleResult editLoad(@RequestParam String id){
-		HandleResult handleResult = new HandleResult();
-		try {
-            SysDeptAuthorityApplyVo vo = service.selectVoById(id);
-		    handleResult.put("vo", vo);
-		} catch (Exception e) {
-		    handleResult.error("获取数据权限申请表信息失败");
-		    logger.error("获取数据权限申请表信息失败", e);
-		}
-		return handleResult;
-		}
-
-    /**
-     * 执行编辑
-     */
-    @RequiresPermissions("XXX:XXX:edit")
-    @Log("编辑数据权限申请表")
-    @RequestMapping("/doEdit")
-    @ResponseBody
-    public  HandleResult doEdit(SysDeptAuthorityApplyVo entity,Model model){
-		HandleResult handleResult = new HandleResult();
-		try {
-		    service.updateVO(entity);
-		    handleResult.success("编辑数据权限申请表成功");
-		} catch (Exception e) {
-		    handleResult.error("编辑数据权限申请表失败");
-		    logger.error("编辑数据权限申请表失败", e);
-		}
-		return handleResult;
+    public HandleResult doAdd(SysDeptAuthorityApplyVo entity) {
+        HandleResult handleResult = new HandleResult();
+        try {
+            service.insertVO(entity);
+            handleResult.success("创建数据权限申请表成功");
+        } catch (Exception e) {
+            handleResult.error("创建数据权限申请表失败");
+            logger.error("创建数据权限申请表失败", e);
+        }
+        return handleResult;
     }
 }
