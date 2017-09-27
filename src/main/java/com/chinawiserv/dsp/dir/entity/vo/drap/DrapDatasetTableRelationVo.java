25cb82e788d1a461dfe6f42236458e3ef9e5766f
liudp <liudp@chinawiserv.com>
2017-09-27 19:05:16 +0800

    需求同步接口

---
+++ src/main/java/com/chinawiserv/dsp/dir/entity/vo/drap/DrapDatasetTableRelationVo.java
@@ -0,0 +1,19 @@
+package com.chinawiserv.dsp.dir.entity.vo.drap;
+
+import com.chinawiserv.dsp.dir.entity.po.drap.DrapDatasetTableRelation;
+
+/**
+ * <p>
+ * 信息资源梳理表关系记录表 Vo对象
+ * </p>
+ *
+ * @author Marke
+ * @since 2017-09-06
+ */
+public class DrapDatasetTableRelationVo extends DrapDatasetTableRelation{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+}
