63edce77b9c18ab6675933fb6993ce4e8deb42de
gongj <gongj@chinawiserv,com>
2017-10-03 11:42:03 +0800

    用户Token验证及发布接口调整

--- src/main/java/com/chinawiserv/dsp/dir/service/api/impl/ApiServiceImpl.java
+++ src/main/java/com/chinawiserv/dsp/dir/service/api/impl/ApiServiceImpl.java
@@ -7,6 +7,7 @@ import com.chinawiserv.dsp.base.entity.po.system.*;
 import com.chinawiserv.dsp.base.mapper.system.SysUserMapper;
 import com.chinawiserv.dsp.dir.entity.po.catalog.DirClassify;
 import com.chinawiserv.dsp.dir.entity.po.catalog.DirDatasetServiceMap;
+import com.chinawiserv.dsp.dir.entity.po.drap.DrapDbTableInfo;
 import com.chinawiserv.dsp.dir.entity.po.service.DirServiceInfo;
 import com.chinawiserv.dsp.dir.mapper.api.ApiMapper;
 import com.chinawiserv.dsp.dir.mapper.api.DirServiceInfoMapper;
@@ -143,6 +144,9 @@ public class ApiServiceImpl implements IApiService {
             return handleResult;
         }else{
             String serviceInfoStr = (String)paramMap.get("serviceInfo"); //获取服务信息的JSON字符串
+            String systemId = (String)paramMap.get("systemId"); //系统ID
+            String dbId = (String)paramMap.get("dbId");  //数据库ID
+            String dirStructure = (String)paramMap.get("dirStructure");  //数据表
             if(null != serviceInfoStr){
                 JSONArray serviceInfoArr = JSONArray.fromObject(serviceInfoStr);
                 //解析JSON字符串,获取服务相关信息
@@ -150,15 +154,13 @@ public class ApiServiceImpl implements IApiService {
 
                     DirServiceInfo dirServiceInfo = new DirServiceInfo();
 
-
-
                     JSONObject serviceInfo = serviceInfoArr.getJSONObject(i);
                     String serviceName = (String)serviceInfo.get("serviceName");
                     String serviceType = (String)serviceInfo.get("serviceType");
                     String serviceUrl = (String)serviceInfo.get("serviceUrl");
                     String requestType = (String)serviceInfo.get("requestType");
                     String dataStyle = (String)serviceInfo.get("dataStyle");
-                    String serviceInfoParams = null;
+                    String serviceInfoParams = "";
                     try{
                         serviceInfoParams = serviceInfo.get("serviceInfoParams").toString();
                     }catch (Exception e){
@@ -168,7 +170,42 @@ public class ApiServiceImpl implements IApiService {
                     String status = (String)serviceInfo.get("status");
                     String dirOrDrapType = (String)serviceInfo.get("dirType"); //目录梳理类型
                     String dirOrDrapTypeId = (String)serviceInfo.get("dirTypeId"); //目录数据集OR梳理数据表ID
+                    /**
+                     * 来自梳理的处理
+                     * */
+                    if("hackle".equalsIgnoreCase(dirOrDrapType)){
+                        /**
+                         * 查询表对应的ID
+                         * */
+                        Map<String,Object> tableParamMap = Maps.newHashMap();
+                        tableParamMap.put("systemId",systemId);
+                        tableParamMap.put("dbId",dbId);
+                        if(null != dirStructure && !dirStructure.contains(",")){
+                            tableParamMap.put("tableName",dirStructure);
+                            DrapDbTableInfo drapDbTableInfo = apiMapper.getTableInfoBySystemIdAndDbId(tableParamMap);
+                            if(null != drapDbTableInfo){
+                                String tableId = drapDbTableInfo.getId();
+                                dirOrDrapTypeId = tableId;
+                            }
+                        }
 
+                        if(dirStructure.contains(",")){
+                            StringBuffer sb = new StringBuffer();
+                            String[] tableNames = dirStructure.split(",");
+                            for (String tableName:tableNames) {
+                                tableParamMap.put("tableName",tableName);
+                                DrapDbTableInfo drapDbTableInfo = apiMapper.getTableInfoBySystemIdAndDbId(tableParamMap);
+                                if(null != drapDbTableInfo){
+                                    String tableId = drapDbTableInfo.getId();
+                                    sb.append(tableId);
+                                    sb.append(",");
+                                }
+                            }
+                            if(null != sb){
+                                dirOrDrapTypeId = sb.toString();
+                            }
+                        }
+                    }
                     /**
                      * 多个源，对ID进行拆分
                      * */
