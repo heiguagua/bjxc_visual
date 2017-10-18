6383d941b65dd28eb78c44ec1f05292cb1268461
lianrf <lianrf@chinawiserv.com>
2017-10-18 17:03:51 +0800

    业务与数据集关系改为中间表

--- src/main/java/com/chinawiserv/dsp/dir/schema/DictConstantMap.java
+++ src/main/java/com/chinawiserv/dsp/dir/schema/DictConstantMap.java
@@ -4,15 +4,12 @@ import org.springframework.util.StringUtils;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Created by lianrongfa on 2017/10/17.
  */
 public class DictConstantMap {
     private static Map<String,String> map=new HashMap<>();
-    private static Lock lock=new ReentrantLock();
     public static String getDictCode(String key){
         if(!StringUtils.isEmpty(key)){
             if(map.containsKey(key)){
@@ -26,14 +23,7 @@ public class DictConstantMap {
 
     public static void putKeyValue(String key,String value){
         if(!StringUtils.isEmpty(key)){
-            try {
-                lock.lock();
-                map.put(key,value);
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
-                lock.unlock();
-            }
+            map.put(key,value);
         }
     }
 }
