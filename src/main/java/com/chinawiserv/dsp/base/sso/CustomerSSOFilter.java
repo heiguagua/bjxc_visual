238f36b20549c6702c90844da253595163940f3d
tangxiong <tangx@chinawiserv.com>
2017-11-17 17:07:34 +0800

    临时调回老版本单点登录

--- src/main/java/com/chinawiserv/dsp/base/sso/CustomerSSOFilter.java
+++ src/main/java/com/chinawiserv/dsp/base/sso/CustomerSSOFilter.java
@@ -150,107 +150,107 @@ public class CustomerSSOFilter extends SSOFilter {
 
     }
     //数据库代替配置文件
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest req = (HttpServletRequest)request;
-        HttpServletResponse resp = (HttpServletResponse)response;
-
-        /**
-         * 不使用浏览器缓存，预防多次访问读取不到参数
-         */
-        resp.setDateHeader("Expires", -1);              //告诉浏览器数据可以缓存多长时间，-1或0表示不缓存
-        resp.setHeader("Cache_Control", "no-cache");    //支持HTTP 1.1，告诉浏览器要不要缓存数据，如“no-cache”
-        resp.setHeader("Pragma", "no-cache");           //支持HTTP 1.0，告诉浏览器要不要缓存数据，如“no-cache”
-
-        Props pros = Props.of(profile);
-        String requestURL = req.getRequestURL().toString();
-        String requestURI = req.getQueryString();
-        String url = requestURL + "?" + requestURI;
-
-        String scode = pros.get("secret_code").stringValue();
-        long timeOut = pros.get("time_out").longValue(TIME_OUT);
-
-        HttpSession session = req.getSession(true);
-
-        Map<String, String> params = urlParams(URLDecoder.decode(Strings.nullToEmpty(requestURI), "UTF-8"));
-
-        String s_t = params.get("s_t");
-        if(!Strings.isNullOrEmpty(s_t)){//带有token 参数，进行解析
-            System.out.println("【带有参数】:"+ s_t);
-            Tokens tk = Tokens.of(s_t);
-            System.out.println("【带有参数】:timeOut:"+ tk.timeout(timeOut)+ "   legal: "+tk.legal(scode));
-            if(!tk.legal(scode) || tk.timeout(timeOut)){
-                System.out.println("【带有参数】----登出");
-                this.logOut(session);
-            }else{//合法
-                if(tk.getAccount() != null){
-                    if(!tk.getAccount().equals(this.curAcc(session))){//当前登录账号不相等
-                        this.logOut(session);
-                        this.setAcc(session, tk.getAccount());
-                    }else{//登录账号相等  ignore
-                    }
-                }else{//登录账号为空
-                    this.logOut(session);
-                }
-            }
-
-            boolean showToken = pros.get("show_url_token").boolValue();
-            if(!showToken){
-                String rto = requestURL+"?";
-                for (String itm : params.keySet()) {
-                    if(!"s_t".equals(itm)){
-                        rto += itm+"=" + URLEncoder.encode(Strings.nullToEmpty(params.get(itm)), "UTF-8")+"&";
-                    }
-                }
-                if(rto.endsWith("&")){
-                    rto = rto.substring(0, rto.length()-1);
-                }
-                if(rto.endsWith("?")){
-                    rto = rto.substring(0, rto.length() -1);
-                }
-                resp.sendRedirect(rto);
-                return;
-            }
-        }
-
-        if(RegExp.match(".{0,}(jp:){1}.{1,}", url)){//跳转到其他系统
-            String arr[] = requestURL.split("jp:");
-            String uri = arr.length > 1?arr[1]:"";
-            uri = RegExp.match("^/.{0,}", uri)?uri.substring(1):uri;
-
-            String[] hes = uri.split("/");
-            String toHost_key = hes.length>0?hes[0]:"";
-//            String host = !Strings.isNullOrEmpty(toHost_key) && RegExp.match("^(hk_).{0,}", toHost_key) ?pros.get("jump_"+toHost_key.replace("hk_", "")).stringValue():pros.get("jump_host").stringValue();
-            String host =null;
-            if(!Strings.isNullOrEmpty(toHost_key) && RegExp.match("^(hk_).{0,}", toHost_key)){
-                String id = toHost_key.replace("hk_", "");
-                try {
-                    SysProductIntegrateVo vo = sysProductIntegrateService.selectVoById(id);
-                    host=vo.getSsoPath();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }else {
-                return;
-            }
-            uri = uri.replace(toHost_key, "");
-
-            //检查是否登录
-            String curAcc = this.curAcc(session);
-            String jurl = host
-                    +(Strings.isNullOrEmpty(uri)?"":(RegExp.match("^/.{0,}$", uri)?uri:("/"+uri) ))
-                    +(Strings.isNullOrEmpty(requestURI)?("?"+"timestamp="+System.currentTimeMillis()):("?"+requestURI+"&timestamp="+System.currentTimeMillis()))
-                    ;
-            if(!Strings.isNullOrEmpty(curAcc)){//不等于空，封装参数
-                String token = Tokens.toToken(curAcc, pros.get("secret_code").stringValue());
-                token = URLEncoder.encode(token, "UTF-8");
-                jurl += ("&s_t="+token);
-            }
-            resp.sendRedirect(jurl);
-            return;
-        }else{
-            chain.doFilter(request, response);
-        }
-
-    }
+//    @Override
+//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+//        HttpServletRequest req = (HttpServletRequest)request;
+//        HttpServletResponse resp = (HttpServletResponse)response;
+//
+//        /**
+//         * 不使用浏览器缓存，预防多次访问读取不到参数
+//         */
+//        resp.setDateHeader("Expires", -1);              //告诉浏览器数据可以缓存多长时间，-1或0表示不缓存
+//        resp.setHeader("Cache_Control", "no-cache");    //支持HTTP 1.1，告诉浏览器要不要缓存数据，如“no-cache”
+//        resp.setHeader("Pragma", "no-cache");           //支持HTTP 1.0，告诉浏览器要不要缓存数据，如“no-cache”
+//
+//        Props pros = Props.of(profile);
+//        String requestURL = req.getRequestURL().toString();
+//        String requestURI = req.getQueryString();
+//        String url = requestURL + "?" + requestURI;
+//
+//        String scode = pros.get("secret_code").stringValue();
+//        long timeOut = pros.get("time_out").longValue(TIME_OUT);
+//
+//        HttpSession session = req.getSession(true);
+//
+//        Map<String, String> params = urlParams(URLDecoder.decode(Strings.nullToEmpty(requestURI), "UTF-8"));
+//
+//        String s_t = params.get("s_t");
+//        if(!Strings.isNullOrEmpty(s_t)){//带有token 参数，进行解析
+//            System.out.println("【带有参数】:"+ s_t);
+//            Tokens tk = Tokens.of(s_t);
+//            System.out.println("【带有参数】:timeOut:"+ tk.timeout(timeOut)+ "   legal: "+tk.legal(scode));
+//            if(!tk.legal(scode) || tk.timeout(timeOut)){
+//                System.out.println("【带有参数】----登出");
+//                this.logOut(session);
+//            }else{//合法
+//                if(tk.getAccount() != null){
+//                    if(!tk.getAccount().equals(this.curAcc(session))){//当前登录账号不相等
+//                        this.logOut(session);
+//                        this.setAcc(session, tk.getAccount());
+//                    }else{//登录账号相等  ignore
+//                    }
+//                }else{//登录账号为空
+//                    this.logOut(session);
+//                }
+//            }
+//
+//            boolean showToken = pros.get("show_url_token").boolValue();
+//            if(!showToken){
+//                String rto = requestURL+"?";
+//                for (String itm : params.keySet()) {
+//                    if(!"s_t".equals(itm)){
+//                        rto += itm+"=" + URLEncoder.encode(Strings.nullToEmpty(params.get(itm)), "UTF-8")+"&";
+//                    }
+//                }
+//                if(rto.endsWith("&")){
+//                    rto = rto.substring(0, rto.length()-1);
+//                }
+//                if(rto.endsWith("?")){
+//                    rto = rto.substring(0, rto.length() -1);
+//                }
+//                resp.sendRedirect(rto);
+//                return;
+//            }
+//        }
+//
+//        if(RegExp.match(".{0,}(jp:){1}.{1,}", url)){//跳转到其他系统
+//            String arr[] = requestURL.split("jp:");
+//            String uri = arr.length > 1?arr[1]:"";
+//            uri = RegExp.match("^/.{0,}", uri)?uri.substring(1):uri;
+//
+//            String[] hes = uri.split("/");
+//            String toHost_key = hes.length>0?hes[0]:"";
+////            String host = !Strings.isNullOrEmpty(toHost_key) && RegExp.match("^(hk_).{0,}", toHost_key) ?pros.get("jump_"+toHost_key.replace("hk_", "")).stringValue():pros.get("jump_host").stringValue();
+//            String host =null;
+//            if(!Strings.isNullOrEmpty(toHost_key) && RegExp.match("^(hk_).{0,}", toHost_key)){
+//                String id = toHost_key.replace("hk_", "");
+//                try {
+//                    SysProductIntegrateVo vo = sysProductIntegrateService.selectVoById(id);
+//                    host=vo.getSsoPath();
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//            }else {
+//                return;
+//            }
+//            uri = uri.replace(toHost_key, "");
+//
+//            //检查是否登录
+//            String curAcc = this.curAcc(session);
+//            String jurl = host
+//                    +(Strings.isNullOrEmpty(uri)?"":(RegExp.match("^/.{0,}$", uri)?uri:("/"+uri) ))
+//                    +(Strings.isNullOrEmpty(requestURI)?("?"+"timestamp="+System.currentTimeMillis()):("?"+requestURI+"&timestamp="+System.currentTimeMillis()))
+//                    ;
+//            if(!Strings.isNullOrEmpty(curAcc)){//不等于空，封装参数
+//                String token = Tokens.toToken(curAcc, pros.get("secret_code").stringValue());
+//                token = URLEncoder.encode(token, "UTF-8");
+//                jurl += ("&s_t="+token);
+//            }
+//            resp.sendRedirect(jurl);
+//            return;
+//        }else{
+//            chain.doFilter(request, response);
+//        }
+//
+//    }
 }
