5599127425da6709faef9ddaadc8b6448cd21fa3
yangd <yangd@chinawiserv.com>
2017-11-21 19:11:33 +0800

    lic 路径问题

--- src/main/java/com/chinawiserv/dsp/base/common/config/Config.java
+++ src/main/java/com/chinawiserv/dsp/base/common/config/Config.java
@@ -1,20 +1,25 @@
 package com.chinawiserv.dsp.base.common.config;
 
+import java.io.File;
+
 /**
  * 静态配置信息
  *  Created by Gaojun.Zhou 2017年4月8日
  */
 public class Config {
-	public static final String SSO_ENCODING = "UTF-8";
-	/**
-	 * license路径,不含服务路径
-	 */
-	public final static String LIC_PATH = "lic";
-	/**
-	 * license名称
-	 */
-	public final static String LIC_NAME = "qinzhi_authorize.lic";
+    public static final String SSO_ENCODING = "UTF-8";
 
+    /**
+     * license名称
+     */
+    public final static String LIC_NAME = "qinzhi_authorize.lic";
+    /**
+     * license路径
+     */
+    public static String getLicPath(){
+        String licPath = System.getProperty("project.path")+ File.separator+"lic";
+        return licPath;
+    }
 
 
 }
