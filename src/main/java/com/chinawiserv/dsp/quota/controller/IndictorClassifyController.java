039f68f1dd7771753ef95ecf93272228fd8b9d27
gaoshch <gaoshch@chinawiserv.com>
2018-05-04 16:32:28 +0800

    controller无用代码删除

--- src/main/java/com/chinawiserv/dsp/quota/controller/IndictorClassifyController.java
+++ src/main/java/com/chinawiserv/dsp/quota/controller/IndictorClassifyController.java
@@ -1,23 +1,12 @@
 package com.chinawiserv.dsp.quota.controller;
 
-import java.util.Map;
-
-import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.baomidou.mybatisplus.plugins.Page;
-import com.chinawiserv.dsp.base.common.anno.Log;
 import com.chinawiserv.dsp.base.controller.common.BaseController;
-import com.chinawiserv.dsp.base.entity.po.common.response.HandleResult;
-import com.chinawiserv.dsp.base.entity.po.common.response.PageResult;
-import com.chinawiserv.dsp.quota.entity.vo.IndictorClassifyVo;
 import com.chinawiserv.dsp.quota.service.IIndictorClassifyService;
 
 /**
@@ -37,113 +26,4 @@ public class IndictorClassifyController extends BaseController {
 	@Autowired
 	private IIndictorClassifyService service;
 
-	@RequiresPermissions("XXX:XXX:list")
-	@RequestMapping("")
-	public String init(@RequestParam Map<String, Object> paramMap) {
-		setCurrentMenuInfo(paramMap);
-		return "XXX/XXX/XXXList";
-	}
-
-	/**
-	 * 分页查询指标分类方式
-	 */
-	@RequiresPermissions("XXX:XXX:list")
-	@RequestMapping("/list")
-	@ResponseBody
-	public PageResult list(@RequestParam Map<String, Object> paramMap) {
-		PageResult pageResult = new PageResult();
-		try {
-			Page<IndictorClassifyVo> page = service.selectVoPage(paramMap);
-			pageResult.setPage(page);
-		} catch (Exception e) {
-			pageResult.error("分页查询指标分类方式出错");
-			logger.error("分页查询指标分类方式出错", e);
-		}
-		return pageResult;
-	}
-
-	/**
-	 * 新增指标分类方式
-	 */
-	@RequiresPermissions("XXX:XXX:add")
-	@RequestMapping("/add")
-	public String add() {
-		return "XXX/XXX/XXXAdd";
-	}
-
-	/**
-	 * 执行新增
-	 */
-	@RequiresPermissions("XXX:XXX:add")
-	@Log("创建指标分类方式")
-	@RequestMapping("/doAdd")
-	@ResponseBody
-	public HandleResult doAdd(IndictorClassifyVo entity) {
-		HandleResult handleResult = new HandleResult();
-		try {
-			service.insertVO(entity);
-			handleResult.success("创建指标分类方式成功");
-		} catch (Exception e) {
-			handleResult.error("创建指标分类方式失败");
-			logger.error("创建指标分类方式失败", e);
-		}
-		return handleResult;
-	}
-
-	/**
-	 * 删除指标分类方式
-	 */
-	@RequiresPermissions("XXX:XXX:delete")
-	@Log("删除指标分类方式")
-	@RequestMapping("/delete")
-	@ResponseBody
-	public HandleResult delete(@RequestParam String id) {
-		// todo 逻辑删除
-		// service.deleteById(id);
-		return new HandleResult().success("删除指标分类方式成功");
-	}
-
-	/**
-	 * 编辑指标分类方式
-	 */
-	@RequiresPermissions("XXX:XXX:edit")
-	@RequestMapping("/edit")
-	public String edit(@RequestParam String id, Model model) {
-		model.addAttribute("id", id);
-		return "XXX/XXX/XXXEdit";
-	}
-
-	@RequiresPermissions("XXX:XXX:edit")
-	@RequestMapping("/editLoad")
-	@ResponseBody
-	public HandleResult editLoad(@RequestParam String id) {
-		HandleResult handleResult = new HandleResult();
-		try {
-			IndictorClassifyVo vo = service.selectVoById(id);
-			handleResult.put("vo", vo);
-		} catch (Exception e) {
-			handleResult.error("获取指标分类方式信息失败");
-			logger.error("获取指标分类方式信息失败", e);
-		}
-		return handleResult;
-	}
-
-	/**
-	 * 执行编辑
-	 */
-	@RequiresPermissions("XXX:XXX:edit")
-	@Log("编辑指标分类方式")
-	@RequestMapping("/doEdit")
-	@ResponseBody
-	public HandleResult doEdit(IndictorClassifyVo entity, Model model) {
-		HandleResult handleResult = new HandleResult();
-		try {
-			service.updateVO(entity);
-			handleResult.success("编辑指标分类方式成功");
-		} catch (Exception e) {
-			handleResult.error("编辑指标分类方式失败");
-			logger.error("编辑指标分类方式失败", e);
-		}
-		return handleResult;
-	}
 }
--- src/main/java/com/chinawiserv/dsp/vm/controller/ChartMenuTemplateController.java
+++ src/main/java/com/chinawiserv/dsp/vm/controller/ChartMenuTemplateController.java
@@ -1,23 +1,12 @@
 package com.chinawiserv.dsp.vm.controller;
 
-import java.util.Map;
-
-import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.baomidou.mybatisplus.plugins.Page;
-import com.chinawiserv.dsp.base.common.anno.Log;
 import com.chinawiserv.dsp.base.controller.common.BaseController;
-import com.chinawiserv.dsp.base.entity.po.common.response.HandleResult;
-import com.chinawiserv.dsp.base.entity.po.common.response.PageResult;
-import com.chinawiserv.dsp.vm.entity.vo.ChartMenuTemplateVo;
 import com.chinawiserv.dsp.vm.service.IChartMenuTemplateService;
 
 /**
@@ -37,113 +26,4 @@ public class ChartMenuTemplateController extends BaseController {
 	@Autowired
 	private IChartMenuTemplateService service;
 
-	@RequiresPermissions("XXX:XXX:list")
-	@RequestMapping("")
-	public String init(@RequestParam Map<String, Object> paramMap) {
-		setCurrentMenuInfo(paramMap);
-		return "XXX/XXX/XXXList";
-	}
-
-	/**
-	 * 分页查询图表与菜单关系模板表
-	 */
-	@RequiresPermissions("XXX:XXX:list")
-	@RequestMapping("/list")
-	@ResponseBody
-	public PageResult list(@RequestParam Map<String, Object> paramMap) {
-		PageResult pageResult = new PageResult();
-		try {
-			Page<ChartMenuTemplateVo> page = service.selectVoPage(paramMap);
-			pageResult.setPage(page);
-		} catch (Exception e) {
-			pageResult.error("分页查询图表与菜单关系模板表出错");
-			logger.error("分页查询图表与菜单关系模板表出错", e);
-		}
-		return pageResult;
-	}
-
-	/**
-	 * 新增图表与菜单关系模板表
-	 */
-	@RequiresPermissions("XXX:XXX:add")
-	@RequestMapping("/add")
-	public String add() {
-		return "XXX/XXX/XXXAdd";
-	}
-
-	/**
-	 * 执行新增
-	 */
-	@RequiresPermissions("XXX:XXX:add")
-	@Log("创建图表与菜单关系模板表")
-	@RequestMapping("/doAdd")
-	@ResponseBody
-	public HandleResult doAdd(ChartMenuTemplateVo entity) {
-		HandleResult handleResult = new HandleResult();
-		try {
-			service.insertVO(entity);
-			handleResult.success("创建图表与菜单关系模板表成功");
-		} catch (Exception e) {
-			handleResult.error("创建图表与菜单关系模板表失败");
-			logger.error("创建图表与菜单关系模板表失败", e);
-		}
-		return handleResult;
-	}
-
-	/**
-	 * 删除图表与菜单关系模板表
-	 */
-	@RequiresPermissions("XXX:XXX:delete")
-	@Log("删除图表与菜单关系模板表")
-	@RequestMapping("/delete")
-	@ResponseBody
-	public HandleResult delete(@RequestParam String id) {
-		// todo 逻辑删除
-		// service.deleteById(id);
-		return new HandleResult().success("删除图表与菜单关系模板表成功");
-	}
-
-	/**
-	 * 编辑图表与菜单关系模板表
-	 */
-	@RequiresPermissions("XXX:XXX:edit")
-	@RequestMapping("/edit")
-	public String edit(@RequestParam String id, Model model) {
-		model.addAttribute("id", id);
-		return "XXX/XXX/XXXEdit";
-	}
-
-	@RequiresPermissions("XXX:XXX:edit")
-	@RequestMapping("/editLoad")
-	@ResponseBody
-	public HandleResult editLoad(@RequestParam String id) {
-		HandleResult handleResult = new HandleResult();
-		try {
-			ChartMenuTemplateVo vo = service.selectVoById(id);
-			handleResult.put("vo", vo);
-		} catch (Exception e) {
-			handleResult.error("获取图表与菜单关系模板表信息失败");
-			logger.error("获取图表与菜单关系模板表信息失败", e);
-		}
-		return handleResult;
-	}
-
-	/**
-	 * 执行编辑
-	 */
-	@RequiresPermissions("XXX:XXX:edit")
-	@Log("编辑图表与菜单关系模板表")
-	@RequestMapping("/doEdit")
-	@ResponseBody
-	public HandleResult doEdit(ChartMenuTemplateVo entity, Model model) {
-		HandleResult handleResult = new HandleResult();
-		try {
-			service.updateVO(entity);
-			handleResult.success("编辑图表与菜单关系模板表成功");
-		} catch (Exception e) {
-			handleResult.error("编辑图表与菜单关系模板表失败");
-			logger.error("编辑图表与菜单关系模板表失败", e);
-		}
-		return handleResult;
-	}
 }
