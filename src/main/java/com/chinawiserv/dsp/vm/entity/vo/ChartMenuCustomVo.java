a873f023ad5a7709cd2e962f96870908628727c7
gaoshch <gaoshch@chinawiserv.com>
2018-05-04 09:26:13 +0800

    图表统计接口修改

--- src/main/java/com/chinawiserv/dsp/vm/entity/vo/ChartMenuCustomVo.java
+++ src/main/java/com/chinawiserv/dsp/vm/entity/vo/ChartMenuCustomVo.java
@@ -10,5 +10,95 @@ import com.chinawiserv.dsp.vm.entity.po.ChartMenuCustom;
  * @author cranky123
  * @since 2018-04-25
  */
-public class ChartMenuCustomVo extends ChartMenuCustom{
+public class ChartMenuCustomVo extends ChartMenuCustom {
+	private String chartCode;
+
+	private String chartName;
+
+	private Integer isNameShow;
+
+	private String chartDesc;
+
+	private String chartType;
+
+	private String chartUrl;
+
+	private String chartTimeType;
+
+	private String chartTimeScope;
+
+	private Integer hasSubIndictor;
+
+	public String getChartCode() {
+		return chartCode;
+	}
+
+	public void setChartCode(String chartCode) {
+		this.chartCode = chartCode;
+	}
+
+	public String getChartName() {
+		return chartName;
+	}
+
+	public void setChartName(String chartName) {
+		this.chartName = chartName;
+	}
+
+	public Integer getIsNameShow() {
+		return isNameShow;
+	}
+
+	public void setIsNameShow(Integer isNameShow) {
+		this.isNameShow = isNameShow;
+	}
+
+	public String getChartDesc() {
+		return chartDesc;
+	}
+
+	public void setChartDesc(String chartDesc) {
+		this.chartDesc = chartDesc;
+	}
+
+	public String getChartType() {
+		return chartType;
+	}
+
+	public void setChartType(String chartType) {
+		this.chartType = chartType;
+	}
+
+	public String getChartUrl() {
+		return chartUrl;
+	}
+
+	public void setChartUrl(String chartUrl) {
+		this.chartUrl = chartUrl;
+	}
+
+	public String getChartTimeType() {
+		return chartTimeType;
+	}
+
+	public void setChartTimeType(String chartTimeType) {
+		this.chartTimeType = chartTimeType;
+	}
+
+	public String getChartTimeScope() {
+		return chartTimeScope;
+	}
+
+	public void setChartTimeScope(String chartTimeScope) {
+		this.chartTimeScope = chartTimeScope;
+	}
+
+	public Integer getHasSubIndictor() {
+		return hasSubIndictor;
+	}
+
+	public void setHasSubIndictor(Integer hasSubIndictor) {
+		this.hasSubIndictor = hasSubIndictor;
+	}
+
 }
